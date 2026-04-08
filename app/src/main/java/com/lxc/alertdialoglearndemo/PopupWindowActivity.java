80857bbf92350a5dd0ce9de6c497a527cf1cdc51
Lxc <a1441982580@163.com>
2026-04-08 10:47:05 +0800

    添加了PopupWindow的使用方法，此处有BUG，动画效果设置后，无法生效

--- app/src/main/java/com/lxc/alertdialoglearndemo/PopupWindowActivity.java
+++ app/src/main/java/com/lxc/alertdialoglearndemo/PopupWindowActivity.java
@@ -1,6 +1,13 @@
 package com.lxc.alertdialoglearndemo;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +16,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PopupWindowActivity extends AppCompatActivity {
-
+    private Button btnShow;
+    private PopupWindow popupWindow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,33 @@ public class PopupWindowActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+    }
+    private void init(){
+        btnShow = findViewById(R.id.btn_show);
+        btnShow.setOnClickListener(v -> {showPopupWindow();});
+    }
+
+    private void showPopupWindow(){
+        View view = getLayoutInflater().inflate(R.layout.view_popup_window,null,false);
+        Button btnXi = view.findViewById(R.id.btn_xixi);
+        Button btnHa = view.findViewById(R.id.btn_haha);
+        btnXi.setOnClickListener(v -> {xiDoSomething();});
+        btnHa.setOnClickListener(v -> {haDoSomething();});
+        popupWindow = new PopupWindow(view, WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,true);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        popupWindow.setAnimationStyle(R.style.PopupWindow_Style);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
+        popupWindow.showAsDropDown(btnShow);
+    }
+
+    private void xiDoSomething(){
+        Toast.makeText(this, "点击了嘻嘻", Toast.LENGTH_SHORT).show();
+        popupWindow.dismiss();
+    }
+    private void haDoSomething(){
+        Toast.makeText(this, "点击了哈哈", Toast.LENGTH_SHORT).show();
+        popupWindow.dismiss();
     }
 }
